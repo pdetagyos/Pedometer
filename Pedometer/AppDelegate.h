4f0074b893f8cfd3fa73c016625ff3dc0c728bbb
Peter C. de Tagyos <peter@ptss.net>
2011-12-11 09:38:47 -0800

    Initial Commit

---
+++ Pedometer/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Pedometer
+//
+//  Created by Peter de Tagyos on 12/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Pedometer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Pedometer
+//
+//  Created by Peter de Tagyos on 12/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
