a4d456475f694df165bbe93c18dfef1c77b428c0
Peter C. de Tagyos <peter@ptss.net>
2011-12-11 17:21:00 -0800

    Initial commit of code. Needs some refinement to properly measure steps.

--- Pedometer/ViewController.h
+++ Pedometer/ViewController.h
@@ -8,6 +8,16 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIAccelerometerDelegate> {
+    float px;
+    float py;
+    float pz;
+
+    int numSteps;
+}
+
+@property (retain, nonatomic) IBOutlet UILabel *stepCountLabel;
+
+- (IBAction)reset:(id)sender;
 
 @end
